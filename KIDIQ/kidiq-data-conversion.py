84ba86127ba3b601280971a90c522a2bc13bc626
Sean Reed <seanreed1111@gmail.com>
2020-10-06 20:09:00 +0000

    addds data conversion file

---
+++ KIDIQ/kidiq-data-conversion.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import json
+
+df = pd.read_csv("https://raw.githubusercontent.com/seanreed1111/MY-RAOS-LECTURE/main/DATA/KidIQ/data/kidiq.csv")
+
+# compose the data
+kid_score = [int(df.kid_score[i]) for i in df.index]
+mom_hs = [int(df.mom_hs[i]) for i in df.index]
+mom_iq = [float(df.mom_iq[i]) for i in df.index]
+mom_work = [int(df.mom_work[i]) for i in df.index]
+mom_age = [int(df.mom_age[i]) for i in df.index]
+
+kidiq = {'kid_score':kid_score, 
+         'mom_hs':mom_hs,
+         'mom_iq':mom_iq,
+         'mom_work':mom_work ,
+         'mom_age':mom_age 
+
+  }
+with open('kidiq.json', 'wt') as f:
+    f.write(json.dumps(kidiq))
--- KIDIQ/kidiqv1.py
+++ KIDIQ/kidiqv1.py
@@ -2,20 +2,20 @@ import os
 import fire
 from cmdstanpy import cmdstan_path, CmdStanModel
 
-def run_model(model_name = 'election.stan', data_name=None):
+def run_model(model_name = 'kidiq.stan', data_name=None):
 
     cwd = os.getcwd()
     model_path = os.path.join(cwd, model_name)
-    election_model = CmdStanModel(stan_file=model_path)
+    kidiq_model = CmdStanModel(stan_file=model_path)
 
     # if model is `model_name.stan`, data file should be called `model_name.json`
 
     data_name = data_name or ("{}.json".format(model_name.split(".")[0]))
 
     data_path = os.path.join(cwd, data_name)
-    election_fit = election_model.sample(data=data_path, output_dir=cwd)
+    kidiq_fit = kidiq_model.sample(data=data_path, output_dir=cwd)
     print(f"model = {model_name} has been sampled successfully")
-    print(election_fit.summary())
+    print(kidiq_fit.summary())
     return  "the model has been sampled successfully"
 
 if __name__ == '__main__':
